b659821a65177e37a9ddc82092319e4235b58515
Nickid2018 <nickid2018@outlook.com>
2022-07-29 22:46:21 +0800

    test

---
+++ src/main/java/io/github/nickid2018/mcde/MCRemapper.java
@@ -0,0 +1,17 @@
+package io.github.nickid2018.mcde;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipFile;
+
+public class MCRemapper {
+
+    // First arg = MC Binary File
+    // Second arg = MC Mapping
+    // Third arg = Destination
+    public static void main(String[] args) throws Exception {
+        try (ZipFile file = new ZipFile(new File(args[0]))) {
+            FileProcessor.process(file, new File(args[1]), new File(args[2]));
+        }
+    }
+}
